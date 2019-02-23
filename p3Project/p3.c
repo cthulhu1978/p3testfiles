5459efb14d13ca51f28eb385e2b328c524fe42c2
Isaac Callison <isaacgcallison@gmail.com>
2019-02-23 00:13:07 -0600

    seems to be working now

--- p3Project/p3.c
+++ p3Project/p3.c
@@ -5,8 +5,9 @@
 #include <math.h>
 #include <pthread.h>
 #include <stdbool.h>
-
-
+#include <time.h>
+clock_t start,end;
+double cpu_time;
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
 struct prime_runner_struct{
@@ -24,10 +25,10 @@ void * checkPrimes(void * arg);
 int main(int argc,char *argv[])
 {
     // required
-    alarm(90);
+    alarm(180);
+    start = clock();
     // how many args are presented:
     int num_args = (argc-1);
-    printf("num args %d\n\n", num_args);
     // array for high and low vals.
     int low_count = 0; int high_count = 0;
     // put values in a high and low array:
@@ -41,13 +42,11 @@ int main(int argc,char *argv[])
       }
     }
     // create enough structs for the threads//
-    struct prime_runner_struct args[num_args];
+    struct prime_runner_struct args[num_args/2];
+
     // create needed threads
-    pthread_t thrdid[num_args];
-
-    int rc, ids[8];
-
-
+    pthread_t thrdid[num_args/2];
+    int rc;
 
     //printf("I am unix process %d\n",getpid());
     for (int i = 0; i < num_args/2; i++)
@@ -59,6 +58,16 @@ int main(int argc,char *argv[])
     {
         pthread_join(thrdid[i],NULL);
     }
+
+    int total_primes = 0;
+    for (size_t i = 0; i < num_args/2; i++) { total_primes += args[i].num_primes; }
+
+    end = clock();
+    cpu_time = ((double) (end - start)) / CLOCKS_PER_SEC;
+    printf("%f  ",cpu_time );
+    printf("%d",total_primes );
+    for (size_t i = 0; i < num_args/2; i++) { printf(" %d", args[i].num_primes );}
+    printf("\n");
 
 
     return 0;
@@ -76,25 +85,22 @@ void * checkPrimes(void * arg)
     thread_count++;
 
     high_range = (int)arg_struct->high;
-    low_range = (int)arg_struct->low;
+    low_range =  (int)arg_struct->low;
 
-    for (int i = high_range; i > low_range; i--) {
+    for (int i = high_range; i >= low_range; i--) {
       bool isPrime = true;
-      for (int j = 2; j < high_range; j++) {
+      for (int j = 2; j <= high_range; j++) {
         if(i % j == 0 && i != j){
             isPrime = false;
-            //printf("num is %d and mod is %d\n", i, j);
+            //printf("i: %d mod j: %d \n",i, j );
           }
         } // end inner for loop
         if(isPrime == true){
-          printf("prime is %d\n", i);
+          //printf("prime is %d\n", i );
+          arg_struct->num_primes += 1;
         }
         isPrime = true;
       } // end second for loop
-
-
-
     pthread_mutex_unlock(&mutex);
-
     pthread_exit(0);
 }
